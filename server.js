3885be5728382d547f9347dda00c8867f604940e
Elton Lac <lacelton@yahoo.com>
2021-04-10 10:49:24 -0700

    Deploying

--- server.js
+++ server.js
@@ -13,9 +13,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 app.use(require("./routes/api.js"));

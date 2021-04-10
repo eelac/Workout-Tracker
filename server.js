b7779dc413855f62ace47fda212c92cd9a960faa
Elton Lac <lacelton@yahoo.com>
2021-04-10 09:39:10 -0700

    Updated server connect

--- server.js
+++ server.js
@@ -13,12 +13,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCrateIndex: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 app.use(require("./routes/api.js"));
 app.use(require("./routes/view.js"));

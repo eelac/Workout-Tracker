e2cba1543f365ad26b9501bf407b0dc0082797e1
Elton Lac <lacelton@yahoo.com>
2021-04-12 18:36:16 -0700

    Updated text

--- models/workout.js
+++ models/workout.js
@@ -12,16 +12,16 @@ const workoutSchema = new Schema({
       type: {
         type: String,
         trim: true,
-        required: "Enter an exercise type",
+        required: "Enter the exercise type",
       },
       name: {
         type: String,
         trim: true,
-        required: "Enter an exercise name",
+        required: "Enter the exercise name",
       },
       duration: {
         type: Number,
-        required: "Enter an exercise duration in minutes",
+        required: "Enter the duration of your exercise",
       },
       weight: {
         type: Number,
